05c7b0233b8b27642f681919a9a3148af98b948a
drainhmrc <daniel.raineri@digital.hmrc>
2018-01-19 12:52:57 +0000

    DC-1315: Great Library Upgrade

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -13,12 +13,8 @@ object MicroServiceBuild extends Build with MicroService {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.16.0",
-    "uk.gov.hmrc" %% "play-authorisation" % "4.3.0",
-    "uk.gov.hmrc" %% "play-health" % "2.1.0",
-    "uk.gov.hmrc" %% "play-config" % "4.3.0",
-    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
-    "uk.gov.hmrc" %% "domain" % "4.1.0"
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "6.15.0",
+    "uk.gov.hmrc" %% "domain" % "5.1.0"
   )
 
   def test(scope: String = "test,it") = Seq(
